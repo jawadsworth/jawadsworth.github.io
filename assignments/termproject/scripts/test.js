5a5b393512be75dfd69f7655c2ed5249aa956715
Jacob Wadsworth <wad10004@byui.edu>
2018-12-13 22:44:13 -0700

    update13.11

--- assignments/termproject/scripts/test.js
+++ assignments/termproject/scripts/test.js
@@ -1,3 +1,5 @@
-$.getJSON( "json/towns.json", function( json ) {
+$.getJSON( "https://jawadsworth.github.io/assignments/termproject/json/towns.json", function( json ) {
     console.log( "JSON Data received, name is " + json.name);
+    var name = json.name;
+    document.getElementById('test').innerHTML = name;
 });

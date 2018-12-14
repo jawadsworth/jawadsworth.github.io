a18b570cc29cf8b67c70e0868d7706908b68cb1c
Jacob Wadsworth <wad10004@byui.edu>
2018-12-13 23:07:22 -0700

    update13.4

--- assignments/termproject/scripts/test.js
+++ assignments/termproject/scripts/test.js
@@ -1,6 +1,19 @@
-$.getJSON( "https://jawadsworth.github.io/assignments/termproject/json/temples.json", function( json ) {
-    console.log( "JSON Data received, name is " + json.temples[0].name);
-    var temple = json['temples'];
-    var temple1 = temple[0].name;
-    document.getElementById('test').innerHTML = temple1;
-});
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';    
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    var projTemples = request.response;
+    showTemples(projTemples);
+
+}
+
+function showTemples(jsonObj) {
+    var pTemples = jsonObj['towns'];
+
+    var name1 = pTemples[0].name;
+
+    document.getElementById('test').innerHTML = name1;
+

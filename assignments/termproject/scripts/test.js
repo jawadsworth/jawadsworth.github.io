6cb88849de34d33ba2ac382c16ff9a6056e160f9
Jacob Wadsworth <wad10004@byui.edu>
2018-12-15 23:35:47 -0700

    update14.8

--- assignments/termproject/scripts/test.js
+++ assignments/termproject/scripts/test.js
@@ -17,7 +17,7 @@ function showTemples1(jsonObj) {
     var phoneh = pTemples[0].telephone;
     var serviceh = pTemples[0].services;
     var historyh = pTemples[0].history;
-    var scheduleh = pTemples[0].schedule.baptism;
+    var baptismh = pTemples[0].baptism;
    
 
     document.getElementById('test').innerHTML = addressh[0];
@@ -31,7 +31,14 @@ function showTemples1(jsonObj) {
     document.getElementById('history1').innerHTML = historyh[0];
     document.getElementById('history2').innerHTML = historyh[1];
     document.getElementById('history3').innerHTML = historyh[2];
-    document.getElementById('sch').innerHTML = scheduleh[0];
+    document.getElementById('b1').innerHTML = baptismh[0];
+    document.getElementById('b2').innerHTML = baptismh[1];
+    document.getElementById('b3').innerHTML = baptismh[2];
+    document.getElementById('b4').innerHTML = baptismh[3];
+    document.getElementById('b5').innerHTML = baptismh[4];
+    document.getElementById('b6').innerHTML = baptismh[5];
+    document.getElementById('b7').innerHTML = baptismh[6];
+    document.getElementById('b8').innerHTML = baptismh[7];
 }
 
 

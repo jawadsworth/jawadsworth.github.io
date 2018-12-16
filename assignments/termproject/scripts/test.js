c51273eb05bc3f9e52cb958a3dff28be4f473091
Jacob Wadsworth <wad10004@byui.edu>
2018-12-15 19:56:28 -0700

    update14.05

--- assignments/termproject/scripts/test.js
+++ assignments/termproject/scripts/test.js
@@ -13,9 +13,11 @@ request.onload = function() {
 function showTemples1(jsonObj) {
     var pTemples = jsonObj['temples'];
     
-    var name1 = pTemples[0].name;
+    var addressh = pTemples[0].address;
 
-    document.getElementById('test').innerHTML = name1;
+    document.getElementById('test').innerHTML = addressh[0];
+    document.getElementById('address1').innerHTML = addressh[0];
+    document.getElementById('address2').innerHTML = addressh[1];
 }
 
 
@@ -40,7 +42,9 @@ function showTemples(jsonObj) {
     var i = document.getElementById('srt').value = document.getElementById('select').value;
     var name1 = pTemples[i].name;
 
-    document.getElementById('test').innerHTML = name1;
+    document.getElementById('test').innerHTML = name1[0];
+    document.getElementById('test1').innerHTML = name1[0];
+    document.getElementById('test2').innerHTML = name1[1];
 }
 
 

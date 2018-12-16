80145deadd6402020d8640127164d06d34906915
Jacob Wadsworth <wad10004@byui.edu>
2018-12-15 23:15:41 -0700

    update14.6

--- assignments/termproject/scripts/test.js
+++ assignments/termproject/scripts/test.js
@@ -14,10 +14,24 @@ function showTemples1(jsonObj) {
     var pTemples = jsonObj['temples'];
     
     var addressh = pTemples[0].address;
+    var phoneh = pTemples[0].telephone;
+    var serviceh = pTemples[0].services;
+    var historyh = pTemples[0].history;
+    var scheduleh = pTemples[0].schedule.baptism;
+   
 
     document.getElementById('test').innerHTML = addressh[0];
     document.getElementById('address1').innerHTML = addressh[0];
     document.getElementById('address2').innerHTML = addressh[1];
+    document.getElementById('phone').innerHTML = phoneh;
+    document.getElementById('service1').innerHTML = serviceh[0];
+    document.getElementById('service2').innerHTML = serviceh[1];
+    document.getElementById('service3').innerHTML = serviceh[2];
+    document.getElementById('service4').innerHTML = serviceh[3];
+    document.getElementById('history1').innerHTML = historyh[0];
+    document.getElementById('history2').innerHTML = historyh[1];
+    document.getElementById('history3').innerHTML = historyh[2];
+    document.getElementById('sch').innerHTML = scheduleh[0];
 }
 
 
@@ -40,11 +54,23 @@ request.onload = function() {
 function showTemples(jsonObj) {
     var pTemples = jsonObj['temples'];
     var i = document.getElementById('srt').value = document.getElementById('select').value;
-    var name1 = pTemples[i].name;
+    var addressh = pTemples[i].address;
+    var phoneh = pTemples[i].telephone;
+    var serviceh = pTemples[i].services;
+    var historyh = pTemples[i].history;
 
-    document.getElementById('test').innerHTML = name1[0];
-    document.getElementById('test1').innerHTML = name1[0];
-    document.getElementById('test2').innerHTML = name1[1];
+    document.getElementById('test').innerHTML = addressh[0];
+    document.getElementById('address1').innerHTML = addressh[0];
+    document.getElementById('address2').innerHTML = addressh[1];
+    document.getElementById('phone').innerHTML = phoneh;
+    document.getElementById('service1').innerHTML = serviceh[0];
+    document.getElementById('service2').innerHTML = serviceh[1];
+    document.getElementById('service3').innerHTML = serviceh[2];
+    document.getElementById('service4').innerHTML = serviceh[3];
+    document.getElementById('history1').innerHTML = historyh[0];
+    document.getElementById('history2').innerHTML = historyh[1];
+    document.getElementById('history3').innerHTML = historyh[2];
+    
 }
 
 
